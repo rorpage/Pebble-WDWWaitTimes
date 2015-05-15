1b132303bb7a76c0448a7bb8c03c17d20c0777e0
Rob Page <rob@rorpage.com>
2015-05-15 15:50:34 -0400

    updated version and capabilities

--- src/app.js
+++ src/app.js
@@ -1,14 +1,28 @@
 var Retriever = require('retriever');
+var Settings = require('settings');
 var UI = require('ui');
 
 // Main Menu
 var buildAndShowMainMenu = function() {
+  Pebble.getTimelineToken(
+    function (token) {
+      console.log("Timeline token: " + token);
+    },
+    function (error) {
+      console.log("Error getting timeline token: " + error);
+    }
+  );
+  
+  Settings.option("userid", 1);
+  Settings.option("apiaccesstoken", "ae9176bd-6396-43b9-88fd-b38b430ab699");
+  
   var items = [];
   items.push(
     { title: "Hours" }, 
     { title: "Ride" }, 
     { title: "Dine" }, 
-    { title: "Plans" }, 
+    { title: "Plans" },
+    { title: "Top Wait Times" },  
     { title: "Weather" }, 
     { title: "What's Near Me?" }
   );
@@ -30,8 +44,10 @@ var buildAndShowMainMenu = function() {
     } else if (e.itemIndex === 3) {
       Retriever.getPlans();
     } else if (e.itemIndex === 4) {
-      Retriever.getWeather();
+      Retriever.getTopWaitTimes();
     } else if (e.itemIndex === 5) {
+      Retriever.getWeather();
+    } else if (e.itemIndex === 6) {
       Retriever.getCurrentPositionForWhatsNearMe();
     }
   });
